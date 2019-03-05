9638e6988b0cbe361316fedfe42b5acf01c62ee0
Rob FunGentoo <higheredbob@gmail.com>
2019-03-05 12:49:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,89 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { ApolloProvider, } from "react-apollo";
-import { BrowserRouter as Router, } from "react-router-dom";
-import styled, { ThemeProvider, } from "styled-components";
-
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect 
+} from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { connect } from "react-redux";
 import client from "./graphql";
-
-import Topbar from "./components/topbar";
+import Setting from './containers/Setting'
+import Home from "./containers/Home";
+import MainLayout from './layouts/withLayout'
+import EmptyLayout from './layouts/EmptyLayout'
 import Posts from "./components/posts";
-import theme from "./styles/theme";
-
 // ------------------------------
 
-const RootStyled = styled.div`
-	flex: 1;
-	align-items: stretch;
-	height: 100vh;
-`;
 
-const Root = () => (
-	<RootStyled>
-		<Topbar />
+const NotFound = () => {
+  return <div>NotFound</div>;
+};
 
-		<Posts />
-	</RootStyled>
-);
+const DashboardRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <MainLayout>
+          <Component {...matchProps} />
+        </MainLayout>
+      )}
+    />
+  );
+};
 
-// ------------------------------
+const EmptyRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <EmptyLayout>
+          <Component {...matchProps} />
+        </EmptyLayout>
+      )}
+    />
+  );
+};
 
-export default () => (
-	<ThemeProvider theme = { theme }>
-		<ApolloProvider client = { client }>
-			<Router>
-				<Root />
-			</Router>
-		</ApolloProvider>
-	</ThemeProvider>
-);
+class App extends Component {
+ 
+  render() {
+    const { settings } = this.props;
+
+    return (
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={settings.theme}>
+        <CssBaseline />
+        <div style={{ height: "100vh" }}>
+          <Router>
+              <Switch>
+                <DashboardRoute path="/setting" component={Setting} />
+                <DashboardRoute path="/dashboard" component={Home} />                
+                <DashboardRoute path="/post/:id" component={DetailPage} />
+                <DashboardRoute path="/" component={Landing} />
+                <DashboardRoute path="/post" component={Posts} />
+                <EmptyRoute component={NotFound} />
+              </Switch>
+            </Router>
+        </div>
+      </MuiThemeProvider>
+      </ApolloProvider>
+    );
+  }
+}
+App.propTypes = {};
+
+const mapStateToProps = state => {
+  return {
+    settings: state.settings,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
